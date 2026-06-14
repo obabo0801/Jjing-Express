bd11cc4db15065ed76e47364c70ecd0cbf62038b
obabo0801 <obabo0801@gmail.com>
2026-06-14 14:18:06 +0900

    update: 68

--- public/js/top.js
+++ public/js/top.js
@@ -4,11 +4,24 @@ export function initTop() {
     const top = $('.top');
 
     on(top, 'contextmenu', blockImageMenu);
+    on(top, 'dragstart', blockDrag);
 }
 
 function blockImageMenu(event) {
     const image = event.target.closest(
         'img, .logo-icon'
+    );
+
+    if (!image) {
+        return;
+    }
+
+    event.preventDefault();
+}
+
+function blockDrag(event) {
+    const image = event.target.closest(
+        '.logo, .logo-icon, img'
     );
 
     if (!image) {
