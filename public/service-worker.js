e4690a84bb1d55e242d77f8ce2ee6be9ed0e351c
obabo0801 <obabo0801@gmail.com>
2026-06-14 12:28:08 +0900

    update: 61

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,19 +2,23 @@ self.addEventListener('notificationclick', (event) => {
     event.notification.close();
 
     event.waitUntil(
-        clients.matchAll({
-            type: 'window',
-            includeUncontrolled: true
-        }).then((clientList) => {
-            for (const client of clientList) {
-                if ('focus' in client) {
-                    return client.focus();
-                }
-            }
-
-            if (clients.openWindow) {
-                return clients.openWindow('/');
-            }
-        })
+        focusOpenClient('/')
     );
 });
+
+async function focusOpenClient(url = '/') {
+    const clientList = await clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+    });
+
+    for (const client of clientList) {
+        if ('focus' in client) {
+            return client.focus();
+        }
+    }
+
+    if (clients.openWindow) {
+        return clients.openWindow(url);
+    }
+}
