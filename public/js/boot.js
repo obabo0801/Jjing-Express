5417b2441491225a63e712ce036aac4e795eee43
obabo0801 <obabo0801@gmail.com>
2026-06-14 18:30:22 +0900

    update: 80

--- public/js/boot.js
+++ public/js/boot.js
@@ -1,8 +1,53 @@
+import { $, on } from './dom.js';
+
 const root = document.documentElement;
+const key = 'theme';
 const darkQuery = '(prefers-color-scheme: dark)';
 
-const theme = matchMedia(darkQuery).matches
-    ? 'dark'
-    : 'light';
+export function initTheme() {
+    const button = $('.theme-button');
 
-root.dataset.theme = theme;
+    setTheme(getTheme(), false);
+    watchTheme();
+
+    on(button, 'click', toggleTheme);
+}
+
+function toggleTheme() {
+    const theme = root.dataset.theme === 'dark'
+        ? 'light'
+        : 'dark';
+
+    setTheme(theme);
+}
+
+function setTheme(theme, save = true) {
+    root.dataset.theme = theme;
+
+    if (save) {
+        localStorage.setItem(key, theme);
+    }
+}
+
+function getTheme() {
+    return localStorage.getItem(key)
+        || systemTheme();
+}
+
+function systemTheme() {
+    return matchMedia(darkQuery).matches
+        ? 'dark'
+        : 'light';
+}
+
+function watchTheme() {
+    const query = matchMedia(darkQuery);
+
+    on(query, 'change', () => {
+        if (localStorage.getItem(key)) {
+            return;
+        }
+
+        setTheme(systemTheme(), false);
+    });
+}
