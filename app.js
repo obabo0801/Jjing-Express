24e11eabd3a984f0a26ff84f826be6b46c43ea0b
obabo0801 <obabo0801@gmail.com>
2026-06-15 01:19:57 +0900

    update: 88

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import https from 'https';
 
 import * as file from '#utils/file';
 import * as log from '#utils/log';
@@ -8,8 +9,19 @@ const app = express();
 const PORT = 3000;
 const PUBLIC_PATH = file.get('public');
 
+const HTTPS_OPTIONS = {
+    key: file.read(
+        'config/private.key'
+    ),
+    cert: file.read(
+        'config/public.crt'
+    )
+};
+
 app.use(express.static(PUBLIC_PATH));
 
-app.listen(PORT, () => {
-    log.info(`http://localhost:${PORT}/`);
+https.createServer(
+    HTTPS_OPTIONS, app
+).listen(PORT, () => {
+    log.info(`https://localhost:${PORT}/`);
 });
