b7c057093cd611a5ec271a72213a4f831c405e24
obabo0801 <obabo0801@gmail.com>
2026-06-17 18:18:10 +0900

    update: 124

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import http from 'http';
 import https from 'https';
 
 import * as file from '#utils/file';
@@ -6,7 +7,8 @@ import * as log from '#utils/log';
 
 const app = express();
 
-const PORT = 443;
+const HTTP_PORT = 80;
+const HTTPS_PORT = 443;
 const PUBLIC_PATH = file.get('public');
 
 const HTTPS_OPTIONS = {
@@ -18,10 +20,29 @@ const HTTPS_OPTIONS = {
     )
 };
 
-app.use(express.static(PUBLIC_PATH));
+app.use(
+    express.static(PUBLIC_PATH)
+);
 
-https.createServer(
-    HTTPS_OPTIONS, app
-).listen(PORT, () => {
-    log.info(`https://localhost:${PORT}/`);
+const isHttps =
+    HTTPS_OPTIONS.key
+    && HTTPS_OPTIONS.cert;
+
+const server = isHttps
+    ? https.createServer(
+        HTTPS_OPTIONS,
+        app
+    )
+    : http.createServer(app);
+
+const port = isHttps
+    ? HTTPS_PORT
+    : HTTP_PORT;
+
+const protocol = isHttps
+    ? 'https'
+    : 'http';
+
+server.listen(port, () => {
+    log.info(`${protocol}://localhost/`);
 });
