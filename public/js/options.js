52c47791d43c2d54df254836df3bd5d14b8bfb83
obabo0801 <obabo0801@gmail.com>
2026-06-20 02:33:35 +0900

    update: 198

--- public/js/options.js
+++ public/js/options.js
@@ -14,10 +14,38 @@ export const DEFAULT_OPTION = {
     settingsType: 'general'
 };
 
+export const OPTION_VALUE = {
+    notifyEnable: ['0', '1'],
+    notifySound: ['0', '1'],
+    screenScale: ['1', '1.1', '1.25', '1.5'],
+    fontScale: ['1', '1.1', '1.25', '1.5'],
+    settingsType: ['general', 'notification']
+};
+
 export function getOption(key, defaultValue) {
     const value = localStorage.getItem(key);
 
-    return value === null
-        ? defaultValue
-        : value;
+    if (value === null) {
+        return defaultValue;
+    }
+
+    const name = Object
+        .entries(OPTION)
+        .find(([, optionKey]) => {
+            return optionKey === key;
+        })?.[0];
+
+    if (!name) {
+        return defaultValue;
+    }
+
+    const values = OPTION_VALUE[name];
+
+    if (!values.includes(value)) {
+        localStorage.removeItem(key);
+
+        return defaultValue;
+    }
+
+    return value;
 }
