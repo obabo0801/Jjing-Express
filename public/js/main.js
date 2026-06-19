5ed5051f0cb3ad747e683cc7a6ca8751299b0f4a
obabo0801 <obabo0801@gmail.com>
2026-06-19 20:07:26 +0900

    update: 193

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,27 @@
-import { initTheme } from './theme.js';
-import { initTop } from './top.js';
-import { initNotification } from './notification.js';
-import { initSettings } from './settings.js';
+import { initTheme }
+    from './theme.js';
+import { initTop }
+    from './top.js';
+import { initNotification }
+    from './notification.js';
+import { initSettings }
+    from './settings.js';
 
 init();
 
 function init() {
-    initReady();
+    ready();
     initTheme();
     initTop();
     initNotification();
     initSettings();
 }
 
-function initReady() {
+function ready() {
     requestAnimationFrame(() => {
-        document.body.classList.add('is-ready');
+        document
+            .body
+            .classList
+            .add('is-ready');
     });
 }
